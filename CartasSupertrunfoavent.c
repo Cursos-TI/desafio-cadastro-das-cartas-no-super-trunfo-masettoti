99f3eb04f876cd9e3c71ffc488143b22c59abedb
masettoti <204113110+masettoti@users.noreply.github.com>
2025-04-27 01:15:01 +0000

    CARTAS SUPER TRUNFO AVENTUREIRO

---
+++ CartasSupertrunfoavent.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+// Desafio Super Trunfo - Cidades
+
+int main() {
+    //Carta 1
+    char estado1 = 'A';
+    char codigo1[4] = "A01";
+    char nomecidade1[60] = "São Paulo";
+    int populacao1 = 12325000;
+    float area1 = 1521.11;
+    float pib1 = 699.28;
+    int pontosturisticos1 = 50;
+
+    //Carta 2
+    char estado2 = 'B';
+    char codigo2[4] = "B02";
+    char nomecidade2[60] = "Rio de Janeiro";
+    int populacao2 = 6748000;
+    float area2 = 1200.25;
+    float pib2 = 300.50;
+    int pontosturisticos2 = 30;
+
+    // Cálculo de Densidade Populacional (habitantes por km²)
+    float densidade_populacional1 = populacao1 / area1;
+    float densidade_populacional2 = populacao2 / area2;
+
+    // Cálculo de PIB per Capita
+    float pib_per_capita1 = pib1 / populacao1;  // PIB em reais
+    float pib_per_capita2 = pib2 / populacao2;  // PIB em reais
+
+    // Exibição dos Dados das Cartas
+    printf("Carta 1: \n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da cidade: %s\n", nomecidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Números de pontos turisticos: %d\n", pontosturisticos1);
+
+    printf("Carta 2: \n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da cidade: %s\n", nomecidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Números de pontos turisticos: %d\n", pontosturisticos2);
+
+
+    
+    return 0;
+
+}
